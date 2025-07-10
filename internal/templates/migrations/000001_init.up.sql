6cfc6e885d07c091542404fe01cb267a4a8fd19f
lntrimitelai <lntri@mitelai.com>
2025-07-10 13:52:54 +0700

    change template

--- internal/templates/migrations/000001_init.up.sql
+++ internal/templates/migrations/000001_init.up.sql
@@ -2,7 +2,8 @@
 CREATE TABLE IF NOT EXISTS schema_migrations (
     id SERIAL PRIMARY KEY,
     applied_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    version VARCHAR(50) NOT NULL UNIQUE
+    version VARCHAR(50) NOT NULL UNIQUE,
+    dirty BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 -- Track who executed migrations and when
@@ -11,6 +12,10 @@ CREATE TABLE IF NOT EXISTS migrations_history (
     executed_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     action VARCHAR(20) NOT NULL,
     version VARCHAR(50) NOT NULL,
-    executed_by VARCHAR(100) NOT NULL
+    executed_by VARCHAR(100) NOT NULL,
+    committed BOOLEAN NOT NULL DEFAULT FALSE,
+    sha256 TEXT NOT NULL,
+    
 );
+
 
